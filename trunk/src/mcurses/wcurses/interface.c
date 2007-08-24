5ee5f011c47dd9c2c5c77e72523f67b2966328da
xyster <xyster@fff445e9-f337-0410-8f88-e99976d769a8>
2007-08-24 03:37:40 +0000

    Initial checkin of wcurses extension module to provide low level Windows hooks for the Windows curses emulation

---
+++ trunk/src/mcurses/wcurses/interface.c
@@ -0,0 +1,100 @@
+#include <windows.h>
+#include "wcurses.h"
+#include "interface.h"
+
+wcurses_t g_wc;
+
+int 
+init(void)
+{
+ return wcurses_init(&g_wc);
+}
+
+int
+deinit(void)
+{
+ return wcurses_deinit(&g_wc);
+} 
+
+int
+noecho(void)
+{
+ return wcurses_noecho(&g_wc);
+}
+
+int 
+echo(void)
+{
+ return wcurses_echo(&g_wc);
+}
+
+int 
+move(int x, int y)
+{
+ COORD xy;
+ xy.X = x;
+ xy.Y = y;
+ return wcurses_move(&g_wc, &xy);
+}
+
+int
+get_attr(void)
+{
+ int attr;
+ 
+ wcurses_get_attr(&g_wc, &attr);
+ 
+ return attr;
+}
+ 
+int 
+set_attr(int attr)
+{
+ return wcurses_set_attr(&g_wc, attr); 
+}
+
+int
+get_screen_size(int *x, int *y)
+{
+ int ret;
+ COORD xy;
+ 
+ ret = wcurses_get_screen_size(&g_wc, &xy);
+ 
+ *x = xy.X;
+ *y = xy.Y;
+ 
+ return ret;
+}
+
+int
+getch(void)
+{
+ int ch;
+ 
+ wcurses_getch(&g_wc, NULL, &ch);
+ 
+ return ch;
+}
+
+int
+write_row_attrs(int x, int y, int len, unsigned short *attrs)
+{
+ COORD xy;
+ 
+ xy.X = x;
+ xy.Y = y;
+ 
+ return wcurses_write_row_attrs(&g_wc, &xy, len, attrs);
+}
+
+int
+write_row_chars(int x, int y, int len, unsigned short *s)
+{
+ COORD xy;
+ 
+ xy.X = x;
+ xy.Y = y;
+ 
+ return wcurses_write_row_chars(&g_wc, &xy, len, s);
+}
---
+++ trunk/src/mcurses/wcurses/wcurses.h
@@ -0,0 +1,26 @@
+#ifndef __WCURSES_H
+#define __WCURSES_H
+
+typedef struct wcurses_t {
+	HANDLE std_i, std_o, std_e;
+	SMALL_RECT extent;
+} wcurses_t;
+
+int wcurses_init(wcurses_t *wc);
+int wcurses_deinit(wcurses_t *wc);
+int wcurses_move(wcurses_t *wc, COORD *xy);
+int wcurses_getxy(wcurses_t *wc, COORD *xy);
+int wcurses_clear_region(wcurses_t *wc, SMALL_RECT *region0);
+int wcurses_print(wcurses_t *wc, char *str);
+int wcurses_get_attr(wcurses_t *wc, int *attr);
+int wcurses_set_attr(wcurses_t *wc, int attr);
+int wcurses_getch(wcurses_t *wc, COORD *xy, int *ch);
+int wcurses_noecho(wcurses_t *wc);
+int wcurses_echo(wcurses_t *wc);
+int wcurses_write_row_attrs(wcurses_t *wc, COORD *xy, int len, WORD *attrs);
+int wcurses_write_row_chars(wcurses_t *wc, COORD *xy, int len, WORD *s);
+int wcurses_delch(wcurses_t *wc, COORD *xy);
+int wcurses_inch(wcurses_t *wc, COORD *xy, TCHAR *ch);
+int wcurses_get_screen_size(wcurses_t *wc, COORD *xy);
+
+#endif
