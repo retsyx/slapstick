d3d8d199e7b3114eef1c3884cf66ba629e13e246
xyster <xyster@fff445e9-f337-0410-8f88-e99976d769a8>
2007-08-29 04:42:41 +0000

    tclock.py works, life.py almost works. Probably need to implement window parent/child relationship to make life.py work.

--- src/mcurses/wcurses.py
+++ src/mcurses/wcurses.py
@@ -30,7 +30,7 @@ COLOR_WHITE = 0x07
 A_NORMAL = 0x07
 A_STANDOUT = 0x70
 A_BOLD = 0x08
-
+A_REVERSE = 0x100
 
 _KEY_SHIFT_1 = 0x80
 _KEY_SHIFT_2 = 0x100
@@ -184,7 +184,13 @@ class Window(object):
         self.attrs = []
         self.buf = []
         self._init_buf()
-        
+    def _attr_grok(self, attr):
+        if attr == None:
+            attr = self.default_attr
+        if attr & A_REVERSE:
+            attr &= 0xFF
+            attr = ((attr&0xf0)>>8) | ((attr&0x0f)<<8)
+        return attr    
     def _init_buf(self):
         # Initialize to rect to ensure drawing doesn't
         # need any fancy logic 
@@ -193,7 +199,6 @@ class Window(object):
         self.dirty = [1] * yy
         self.attrs = [[self.default_attr] * xx for yyy in xrange(yy)]
         self.buf = [[self.default_char] * xx for yyy in xrange(yy)]
-                
     def _fit(self, length):
         x, y = self.xy
         sx = self.rect[2] - self.rect[0]
@@ -207,7 +212,6 @@ class Window(object):
         if x + length >= len(buf[y]):
             attrs[y] += [self.default_attr] * (x+length-len(attrs[y])) 
             buf[y] += [self.default_char] * (x+length-len(buf[y]))
-    
     def _move_x(self, off):
         x, y = self.xy
         x += off
@@ -237,8 +241,7 @@ class Window(object):
         elif nCount == 3:
             ch = y
             y = None
-        if attr == None:
-            attr = self.default_attr
+        attr = self._attr_grok(attr)    
         if x != None and y != None:
             self.move(y, x)
         if type(ch) == type(''): ch = ord(ch)
@@ -249,7 +252,6 @@ class Window(object):
         self.attrs[y][x] = attr
         self.buf[y][x] = ch
         self._move_x(1)
-            
     def addstr(self, y, x=None, s=None, attr=None):
         # emulate silly parameter combinations
         # s             3
@@ -266,13 +268,12 @@ class Window(object):
         elif nCount == 3:
             s = y
             y = None
-        if attr == None:
-            attr = self.default_attr
+        attr = self._attr_grok(attr)
         if x != None and y != None:
             self.move(y, x)
         self._fit(len(s))
         x, y = self.xy
-        new_attr = [attr for ch in s]
+        new_attr = [attr] * len(s)
         new_buf = [ord(ch) for ch in s]
         if self.attrs[y][x:x+len(s)] != new_attr:
             self.attrs[y][x:x+len(s)] = new_attr
@@ -281,10 +282,12 @@ class Window(object):
             self.buf[y][x:x+len(s)] = new_buf
             self.dirty[y] = 1
         self._move_x(len(s))
+    def attroff(self, attr):
+        self.default_attr &= ~attr
+    def attron(self, attr):
+        self.default_attr |= attr    
     def attrset(self, attr):
-        if not attr:
-            attr = A_NORMAL
-        self.default_attr = attr
+        self.default_attr = self._attr_grok(attr)
     def bkgdset(self, ch, attr=None):
         if not attr:
             attr = ch
@@ -354,16 +357,7 @@ class Window(object):
             nlines, ncols = None, None
         x += self.rect[0]
         y += self.rect[1]
-        if not nlines:
-            sx, sy = self.rect[2:4]
-        else:
-            sx = x + ncols
-            sy = y + nlines 
-        if sx >= self.rect[2]:
-            sx = self.rect[2] - 1
-        if sy >= self.rect[3]:
-            sy = self.rect[3] - 1   
-        return Window((x, y, sx, sy))
+        return self.subwin(nlines, ncols, y, x)
     def erase(self):
         self._init_buf()
     def getbegyx(self):
@@ -376,6 +370,7 @@ class Window(object):
             ch = _KEY_SHIFT_2 + ord(msvcrt.getch())
         return ch
     def getch(self, y=None, x=None):
+        if not self.pad: self.refresh()
         if x and y:
             self.move(x, y)
         delay = self.delay
@@ -465,7 +460,6 @@ class Window(object):
             if len([tt for tt in t if tt != None]): raise Exception, 'Must specify all parameters or none'
         if self.pad and None in t:
             raise Exception, 'Must specify parameters for pad'
-        # XXX handle pad drawing
         if pminrow < 0: pminrow = 0
         if pmincol < 0: pmincol = 0
         if sminrow < 0: sminrow = 0
@@ -496,7 +490,6 @@ class Window(object):
         wc.delete_short_array(a)        
         # restore cursor
         wc.move(self.xy[0] + self.rect[0], self.xy[1] + self.rect[1])
-            
     def debug_refresh(self):
         i = 0
         for ln in self.buf:
@@ -504,8 +497,26 @@ class Window(object):
             print ''.join([chr(ch) for ch in ln])
             #print '\n'
             i += 1
+    def subwin(self, nlines, ncols, y=None, x=None):
+        if y == None:
+            y, x = nlines, ncols
+            nlines, ncols = None, None
+        if not nlines:
+            sx, sy = self.rect[2:4]
+            sx -= x
+            sy -= y
+        else:
+            sx = x + ncols
+            sy = y + nlines 
+        if sx >= self.rect[2]:
+            sx = self.rect[2] - 1
+        if sy >= self.rect[3]:
+            sy = self.rect[3] - 1   
+        return Window((x, y, sx, sy))
     def timeout(self, delay):
         self.delay = delay
+    def touchwin(self):
+        self.dirty = [1] * len(self.dirty)
     def vline(self, y, x, ch=None, n=None):     
         # emulate silly parameter combinations
         # ch, n         2
@@ -537,23 +548,10 @@ class Window(object):
 class error:
     pass
 
-def rectangle(win, uly, ulx, lry, lrx):
-    """Draw a rectangle with corners at the provided upper-left
-    and lower-right coordinates.
-    """
-    win.vline(uly+1, ulx, ACS_VLINE, lry - uly - 1)
-    win.hline(uly, ulx+1, ACS_HLINE, lrx - ulx - 1)
-    win.hline(lry, ulx+1, ACS_HLINE, lrx - ulx - 1)
-    win.vline(uly+1, lrx, ACS_VLINE, lry - uly - 1)
-    win.addch(uly, ulx, ACS_ULCORNER)
-    win.addch(uly, lrx, ACS_URCORNER)
-    win.addch(lry, lrx, ACS_LRCORNER)
-    win.addch(lry, ulx, ACS_LLCORNER)
-    
 def test():
     win = initscr()
     win.refresh()
-    rectangle(win, 2, 2, 10, 15)
+    win.box()
     win.addstr(4, 4, 'Hello World', A_NORMAL)
     win.addstr(5, 4, 'Goodbye', A_STANDOUT)
     win.addstr(6, 4, '0123456789abcdef0123456789abcdef', A_NORMAL)
