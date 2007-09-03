1d3ebcedaf2779b2100cf65524e7f775cb2e3ab9
xyster <xyster@fff445e9-f337-0410-8f88-e99976d769a8>
2007-09-03 20:38:14 +0000

    Add utilities folder with keyboard utility in it.

---
+++ trunk/util/keys.py
@@ -0,0 +1,25 @@
+import mcurses as curses
+
+def main(win):
+    cd = [d for d in dir(curses) if 'KEY' in d]
+    ad = [d for d in dir(curses.ascii)]
+    
+    win.addstr(0, 0, 'press a to exit')
+    ch = 65
+    while ch != 97:
+        ch = win.getch()
+        cdef = ''
+        for d in cd:
+            if ch == eval('curses.'+d):
+                cdef = 'curses.'+d
+                break
+        if cdef == '':
+            for d in ad:
+                if ch == eval('curses.ascii.'+d):
+                    cdef = 'curses.ascii.'+d
+                    break
+        s = '%d %s' % (ch, cdef)        
+        win.insertln()
+        win.addstr(0, 0, s)
+        
+curses.wrapper(main)
