db4e1732fad945d4d4166b344aeb108318b50e21
xyster <xyster@fff445e9-f337-0410-8f88-e99976d769a8>
2007-09-09 23:10:37 +0000

    fix up indent to something semi-regular

--- trunk/src/mselect/wselect/wselect.c
+++ trunk/src/mselect/wselect/wselect.c
@@ -6,116 +6,116 @@
 #define OFFSET 1
 
 typedef enum {
-HND_TYPE_INVALID,
-HND_TYPE_CONSOLE,
-HND_TYPE_PIPE,
-HND_TYPE_FILE,
-HND_TYPE_SOCKET,
+    HND_TYPE_INVALID,
+    HND_TYPE_CONSOLE,
+    HND_TYPE_PIPE,
+    HND_TYPE_FILE,
+    HND_TYPE_SOCKET,
 } hnd_type;
 
 struct mhnd {
- hnd_type type;
- HANDLE hnd;
+    hnd_type type;
+    HANDLE hnd;
 };
  
 static PyObject *
 wselect(PyObject *self, PyObject *args)
-/*int nof_fds, int *fds, int timeout_ms)*/
 {
- struct mhnd *hnds;
- BOOL res;
- DWORD bytes_avail, type;
- int i, len, timeout_ms, timo;
- PyObject *list, *item;
+    struct mhnd *hnds;
+    BOOL res;
+    DWORD bytes_avail, type;
+    int i, len, timeout_ms, timo;
+    PyObject *list, *item;
  
- /* XXX hideous hack to pretend we have select for the purposes
-    XXX of our app
- */
- hnds = NULL;
- i = -1;
+    /* XXX hideous hack to pretend we have select for the purposes
+        XXX of our app
+    */
+    hnds = NULL;
+    i = -OFFSET;
  
- if (!PyArg_ParseTuple(args, "Oi", &list, &timeout_ms))
-    return NULL;
+    if (!PyArg_ParseTuple(args, "Oi", &list, &timeout_ms))
+        return NULL;
  
- if (timeout_ms < 0)
-    timo = 100;
- else
-    timo = timeout_ms;
+    if (timeout_ms < 0)
+        timo = 100;
+    else
+        timo = timeout_ms;
  
- if (!PyList_Check(list))
-    return NULL;
+    if (!PyList_Check(list))
+        return NULL;
  
- len = (int)PyList_Size(list);
- if (!len) goto out;
+    len = (int)PyList_Size(list);
+    if (!len) goto out;
     
- hnds = PyMem_Malloc(sizeof(struct mhnd)*len);
- if (!hnds)
-    return NULL;
+    hnds = PyMem_Malloc(sizeof(struct mhnd)*len);
+    if (!hnds)
+        return NULL;
   
- for (i = 0; i < len; i++)
-   {
-    item = PyList_GetItem(list, i);
-    if (!PyInt_Check(item))
-     {
-      PyMem_Free(hnds);
-      return NULL;
-     }
+    for (i = 0; i < len; i++)
+    {
+        item = PyList_GetItem(list, i);
+        if (!PyInt_Check(item))
+        {
+            PyMem_Free(hnds);
+            return NULL;
+        }
      
-    hnds[i].hnd = (HANDLE)PyInt_AsLong(item);
-    type = GetFileType(hnds[i].hnd);
-    switch (type)
-     {
-      case FILE_TYPE_CHAR:
-        hnds[i].type = HND_TYPE_CONSOLE;
-        break;
-      case FILE_TYPE_PIPE:
-        if (hnds[i].hnd == GetStdHandle(STD_INPUT_HANDLE))
-          hnds[i].type = HND_TYPE_CONSOLE;
-        else
-          hnds[i].type = HND_TYPE_PIPE;
-        break;
-      case FILE_TYPE_DISK:
-        hnds[i].type = HND_TYPE_FILE;
-        break;
-      default:
-        hnds[i].type = HND_TYPE_SOCKET; /* XXX this is probably a bad assumption */        
-     } 
-   }
+        hnds[i].hnd = (HANDLE)PyInt_AsLong(item);
+        type = GetFileType(hnds[i].hnd);
+        switch (type)
+        {
+            case FILE_TYPE_CHAR:
+                hnds[i].type = HND_TYPE_CONSOLE;
+                break;
+            case FILE_TYPE_PIPE:
+                if (hnds[i].hnd == GetStdHandle(STD_INPUT_HANDLE))
+                    hnds[i].type = HND_TYPE_CONSOLE;
+                else
+                    hnds[i].type = HND_TYPE_PIPE;
+                break;
+            case FILE_TYPE_DISK:
+                hnds[i].type = HND_TYPE_FILE;
+                break;
+            default:
+                hnds[i].type = HND_TYPE_SOCKET; /* XXX this is probably a bad assumption */        
+        } 
+    }
     
- do {
-     for (i = 0; i < len; i++)
-       {
-        switch (hnds[i].type)
-         {
-          case HND_TYPE_CONSOLE:
-            if (_kbhit()) goto out;
-            break;
-          case HND_TYPE_PIPE:
-            bytes_avail = 0;
-            res = PeekNamedPipe(hnds[i].hnd, NULL, 0, NULL, &bytes_avail, NULL);
-            if (bytes_avail) goto out;
-            break;
-          default: /* XXX don't handle anything else */
-            break;  
-         }
-       }  
-     
-     if (timo >= 100)
-        Sleep(100);
-     else
-        Sleep(timo);
+    do 
+    {
+        for (i = 0; i < len; i++)
+        {
+            switch (hnds[i].type)
+            {
+                case HND_TYPE_CONSOLE:
+                    if (_kbhit()) goto out;
+                break;
+                case HND_TYPE_PIPE:
+                    bytes_avail = 0;
+                    res = PeekNamedPipe(hnds[i].hnd, NULL, 0, NULL, &bytes_avail, NULL);
+                    if (bytes_avail) goto out;
+                    break;
+                default: /* XXX don't handle anything else */
+                    break;  
+            }
+        }  
+    
+        if (timo >= 100)
+            Sleep(100);
+        else
+            Sleep(timo);
            
-     if (timeout_ms > 0)
-        timo -= 100;
+        if (timeout_ms > 0)
+            timo -= 100;
         
     } while (timo > 0);
 
-i = -1;
+    i = -1;
 
 out:
- if (hnds) PyMem_Free(hnds);
+    if (hnds) PyMem_Free(hnds);
  
- return Py_BuildValue("i", OFFSET + i);   
+    return Py_BuildValue("i", OFFSET + i);   
 }
 
 static PyMethodDef _functions[] = {
