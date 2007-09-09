2574d896096dcab34dd9330c7d01ab41cf662309
xyster <xyster@fff445e9-f337-0410-8f88-e99976d769a8>
2007-09-09 22:55:50 +0000

    Turn wselect into a native Python extension so we not longer need swig. Also make it slightly more general.

--- trunk/src/mselect/wselect/wselect.c
+++ trunk/src/mselect/wselect/wselect.c
@@ -1,188 +1,130 @@
+#include <Python.h>
 #include <conio.h>
 #include <time.h>
 #include <windows.h>
-#include <Python.h>
-#include "wselect.h"
 
-#if 0
+#define OFFSET 1
 
-#define WS_MAX_HANDLES MAX_HANDLES
+typedef enum {
+HND_TYPE_INVALID,
+HND_TYPE_CONSOLE,
+HND_TYPE_PIPE,
+HND_TYPE_FILE,
+HND_TYPE_SOCKET,
+} hnd_type;
 
-typedef struct wselect_data_t {
-	HANDLE hnd;
-	HANDLE evt_p;
-	HANDLE evt_c;
-	HANDLE thread;
-} wselect_data_t;
-
-struct {
-	wselect_data_t wsd[WS_MAX_HANDLES]; 
-} g_ws;
-
-int inited;
-
-DWORD WINAPI 
-threadProc(LPVOID lpParameter)
+struct mhnd {
+ hnd_type type;
+ HANDLE hnd;
+};
+ 
+static PyObject *
+wselect(PyObject *self, PyObject *args)
+/*int nof_fds, int *fds, int timeout_ms)*/
 {
- wselect_data_t *wsd;
+ struct mhnd *hnds;
  BOOL res;
- DWORD bytes_read;
- char buf[1];
- DWORD nof_read;
- INPUT_RECORD inp[1];
- 
- wsd = (wselect_data_t *)lpParameter;
- 
- do {
-	 WaitForSingleObject(wsd->evt_p, INFINITE);
-     res = ReadFile(wsd->hnd, buf, 0, &bytes_read, NULL);
-     //printf("res, bytes_read %d %d\n", res, bytes_read);
-     /* Extract more info for console */
-	 inp[0].EventType = 0;
-	 res = PeekConsoleInput(wsd->hnd, inp, 1, &nof_read);
-	 if (res)
-	  {
-	   //printf("nof_read=%d, 0x%X\n", nof_read, inp[0].EventType); 
-	   if (!nof_read)
-	    {
-	     continue;
-	    }
-	   if (inp[0].EventType == KEY_EVENT)
-	    {
-	     if (!inp[0].Event.KeyEvent.bKeyDown)
-	      {
-           continue;
-	      }
-	    } 
-	  }
-	 res = TRUE; 
-	 SetEvent(wsd->evt_c);
-    } while (res);
-  
- return 0;
-}
-
-
-int
-init(void)
-{
- int i;
- wselect_data_t *wsd;
- 
- if (inited) return 0;
- 
- /* Setup threads
- */
- for (i = 0; i < WS_MAX_HANDLES; i++)
-   {
-    wsd = &g_ws.wsd[i]; 
-	wsd->hnd = INVALID_HANDLE_VALUE;
-	wsd->evt_p = CreateEvent(NULL, FALSE, FALSE, NULL);
-	wsd->evt_c = CreateEvent(NULL, TRUE, FALSE, NULL);
-	wsd->thread = CreateThread(NULL, 0, threadProc, wsd, 0, NULL);
- 
-	if (wsd->evt_p == INVALID_HANDLE_VALUE  ||
-		wsd->evt_c == INVALID_HANDLE_VALUE  ||
-		wsd->thread == INVALID_HANDLE_VALUE)
-	 {
-	  CloseHandle(wsd->evt_p);
-      CloseHandle(wsd->evt_c);
-      TerminateThread(wsd->thread, 0);
-      CloseHandle(wsd->thread);
-      return -1;
-     }
-   }
- 
- inited = 1;
-     
- return 0; 
-}
-
-int
-deinit(void)
-{
- int i;
- wselect_data_t *wsd;
- 
- if (!inited) return 0;
- 
- for (i = 0; i < WS_MAX_HANDLES; i++)
-   {
-    wsd = &g_ws.wsd[i];
-    wsd->hnd = INVALID_HANDLE_VALUE;
-    CloseHandle(wsd->evt_p);
-    CloseHandle(wsd->evt_c);
-    TerminateThread(wsd->thread, 0);
-    CloseHandle(wsd->thread);
-   } 
- return 0;
-}
-
-int
-wselect1(int nof_fds, int *fds, int timeout_ms)
-{
- int i;
- HANDLE evts[WS_MAX_HANDLES];
- DWORD ret;
- if (!inited) return -1;
- 
- if (nof_fds > WS_MAX_HANDLES) return -1;
- for (i = 0; i < nof_fds; i++)
-   {
-    //g_ws.wsd[i].hnd = (HANDLE)_get_osfhandle(fds[i]); // XXX Crashes for an unknown reason
-    g_ws.wsd[i].hnd = (HANDLE)fds[i];
-    evts[i] = g_ws.wsd[i].evt_c;
-    ResetEvent(g_ws.wsd[i].evt_c);
-    SetEvent(g_ws.wsd[i].evt_p);
-   }
-
- ret = WaitForMultipleObjects(nof_fds, evts, FALSE, timeout_ms);
- 
- if (ret == WAIT_FAILED)
-  {
-    return -1;
-  }
- if (ret == WAIT_TIMEOUT)
-  {
-    return 0;
-  }
- return ret - WAIT_OBJECT_0 + OFFSET; // +1 to differentiate from timeout
-}
-
-#endif
-
-int
-wselect(int nof_fds, int *fds, int timeout_ms)
-{
- HANDLE c_hnd, p_hnd;
- BOOL res;
- DWORD bytes_avail;
+ DWORD bytes_avail, type;
+ int i, len, timeout_ms, timo;
+ PyObject *list, *item;
  
  /* XXX hideous hack to pretend we have select for the purposes
     XXX of our app
-    XXX assume first fd is console, second fd is pipe
  */
- if (nof_fds > 0)
-    c_hnd = (HANDLE)fds[0];
- else
-    return 0;   
- if (nof_fds > 1)
-    p_hnd = (HANDLE)fds[1];
- else
-    p_hnd = INVALID_HANDLE_VALUE;
- do {
-     if (p_hnd != INVALID_HANDLE_VALUE)
-      {
-       bytes_avail = 0;
-       res = PeekNamedPipe(p_hnd, NULL, 0, NULL, &bytes_avail, NULL);
-       if (bytes_avail) return 1+OFFSET;
-      } 
-     if (_kbhit()) return 0+OFFSET;
-     Sleep(100);
-     timeout_ms -= 100;
-    } while (timeout_ms > 0);
+ hnds = NULL;
+ i = -1;
  
- return 0;   
+ if (!PyArg_ParseTuple(args, "Oi", &list, &timeout_ms))
+    return NULL;
+ 
+ if (timeout_ms < 0)
+    timo = 100;
+ else
+    timo = timeout_ms;
+ 
+ if (!PyList_Check(list))
+    return NULL;
+ 
+ len = (int)PyList_Size(list);
+ if (!len) goto out;
+    
+ hnds = PyMem_Malloc(sizeof(struct mhnd)*len);
+ if (!hnds)
+    return NULL;
+  
+ for (i = 0; i < len; i++)
+   {
+    item = PyList_GetItem(list, i);
+    if (!PyInt_Check(item))
+     {
+      PyMem_Free(hnds);
+      return NULL;
+     }
+     
+    hnds[i].hnd = (HANDLE)PyInt_AsLong(item);
+    type = GetFileType(hnds[i].hnd);
+    switch (type)
+     {
+      case FILE_TYPE_CHAR:
+        hnds[i].type = HND_TYPE_CONSOLE;
+        break;
+      case FILE_TYPE_PIPE:
+        if (hnds[i].hnd == GetStdHandle(STD_INPUT_HANDLE))
+          hnds[i].type = HND_TYPE_CONSOLE;
+        else
+          hnds[i].type = HND_TYPE_PIPE;
+        break;
+      case FILE_TYPE_DISK:
+        hnds[i].type = HND_TYPE_FILE;
+        break;
+      default:
+        hnds[i].type = HND_TYPE_SOCKET; /* XXX this is probably a bad assumption */        
+     } 
+   }
+    
+ do {
+     for (i = 0; i < len; i++)
+       {
+        switch (hnds[i].type)
+         {
+          case HND_TYPE_CONSOLE:
+            if (_kbhit()) goto out;
+            break;
+          case HND_TYPE_PIPE:
+            bytes_avail = 0;
+            res = PeekNamedPipe(hnds[i].hnd, NULL, 0, NULL, &bytes_avail, NULL);
+            if (bytes_avail) goto out;
+            break;
+          default: /* XXX don't handle anything else */
+            break;  
+         }
+       }  
+     
+     if (timo >= 100)
+        Sleep(100);
+     else
+        Sleep(timo);
+           
+     if (timeout_ms > 0)
+        timo -= 100;
+        
+    } while (timo > 0);
+
+i = -1;
+
+out:
+ if (hnds) PyMem_Free(hnds);
+ 
+ return Py_BuildValue("i", OFFSET + i);   
 }
 
+static PyMethodDef _functions[] = {
+    {"select", (PyCFunction)wselect, 1},
+    {NULL, NULL}
+};
 
+DL_EXPORT(void)
+init_wselect_c()
+{
+    Py_InitModule("_wselect_c", _functions);
+}
